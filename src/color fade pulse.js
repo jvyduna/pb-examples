caea2a79762b69ee8d22c395718acc343a7b28cc
Ben Hencke <brainstar@gmail.com>
2020-10-29 11:16:27 -0700

    add a slider to control the number of pulses

--- src/color fade pulse.js
+++ src/color fade pulse.js
@@ -11,6 +11,12 @@ export function beforeRender(delta) {
   t3 = time(.1)  // White / desaturation movement
 }
 
+export var pulses = 4
+//set up a slider to change the number of pulses from 1-10
+export function sliderPulses(v) {
+  pulses = 1 + floor(v*9)
+}
+
 export function render(index) {
   // When you see a function using time as a `- t1` phase shift, this is moving
   // to the right.
@@ -18,15 +24,15 @@ export function render(index) {
 
   /*
     This creates the pulses themselves. A `+ t2` indicates these will be moving 
-    to the left. The `* 4` makes them more frequent in the strip. In fact, you 
-    an think of this as "having 4 pulses visible at any given time."
+    to the left. The `* pulses` makes them more frequent in the strip. In fact, you 
+    an think of this as "having this many pulses visible at any given time."
   */
-  v = triangle(index / pixelCount * 4 + t2) 
+  v = triangle(index / pixelCount * pulses + t2) 
   v = v * v * v * v
     
-  // Every few pulses will be whiter (low saturation). Each pulse will very 
+  // Every other pulse will be whiter (low saturation). Each pulse will very 
   // slowly alternate between a whitish pulse and deeper saturated hues.
-  s = wave(index / pixelCount / 2 + t3)
+  s = wave(index / pixelCount / 2 * pulses + t3)
   
   hsv(h, s, v)
 }
